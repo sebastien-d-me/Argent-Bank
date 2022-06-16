89d060037d498f019e6c0da7d60db8ca6ce12d33
sebastien-d-fr <contact@sebastien-d.me>
2022-06-16 14:49:23 +0200

    Bug fix

--- Site/src/pages/Logout/Logout.js
+++ Site/src/pages/Logout/Logout.js
@@ -1,14 +1,22 @@
 import { useDispatch } from "react-redux";
 import { getToken } from "../../features/Token/token";
 import { Navigate } from "react-router-dom";
+import { getFirstName } from "../../features/User/firstName";
+import { getLastName } from "../../features/User/lastName";
+import { useEffect } from "react";
 
 
 function Logout() {
     // Change the token
     const dispatch = useDispatch();
-    dispatch(getToken(0));
 
-
+    useEffect(() => {
+        dispatch(getToken(0));
+        dispatch(getFirstName(""));
+        dispatch(getLastName(""));
+    });
+    
+    
     // Redirection
     return <Navigate to="/" /> 
 }
