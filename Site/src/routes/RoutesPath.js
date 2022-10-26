f518451ffff016851d4c685aae63cfb5610ccb31
sebastien-d-fr <contact@sebastien-d.me>
2022-10-26 04:14:09 +0200

    Update RoutesPath.js

--- Site/src/routes/RoutesPath.js
+++ Site/src/routes/RoutesPath.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import Accueil from "../pages/Accueil/Accueil";
 import Login from "../pages/Login/Login";
@@ -9,17 +9,18 @@ import Erreur404 from "../pages/404/404";
 
 function RoutesPath() {
     return(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Layout>
                 <Routes>
-                    <Route path="/" element={<Accueil />} />
+                    <Route element={<Navigate replace to="/home" />} path="/" />
+                    <Route path="/home" element={<Accueil />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/profil" element={<Profil />} />
                     <Route path="/logout" element={<Logout />} />
                     <Route path="*" element={<Erreur404 />} />
                 </Routes>
             </Layout>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
